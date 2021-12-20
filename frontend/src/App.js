6c4f2a4a719c3b024eee308d491d5c0c7b60b556
Junhua9981 <lichu9981@gmail.com>
2021-12-20 16:50:32 +0800

    feat: adminPage template anddfolder sideBar template done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,6 @@ import UserIdListTable from "./components/AdminPage/UserIdListTable";
 import FolderList from "./components/FolderPage/FolderList";
 
 function App() {
-
   let localDarkMode = localStorage.getItem("darkMode");
   const [darkMode, setDarkMode] = useState(localDarkMode);
 
@@ -24,7 +23,7 @@ function App() {
     setDarkMode(darkMode);
     localStorage.setItem("darkMode", darkMode);
     console.log("App is " + darkMode);
-  }
+  };
 
   const theme = createTheme({
     palette: {
@@ -33,7 +32,7 @@ function App() {
         main: "#37AEF2",
         light: "#37AEF2",
         dark: "#1B92D1",
-        contrastText: "#fff"
+        contrastText: "#fff",
       },
     },
     mixins: {
@@ -42,8 +41,7 @@ function App() {
   });
 
   return (
-    
-    <ThemeProvider theme={theme} >
+    <ThemeProvider theme={theme}>
       <Paper elevation={0}>
         <Header isLogin={false} onChangeDarkMode={changeDarkMode} />
         <Routes>
@@ -53,10 +51,10 @@ function App() {
           <Route exact path="newDiary" element={<NewDiaryPage />} />
           <Route exact path="calenderSearch" element={<CalenderSearchPage />} />
           <Route exact path="folderPage" element={<FolderPage />} />
-          <Route exact path="test" element={<UserIdListTable/>} />
+          <Route exact path="test" element={<UserIdListTable />} />
         </Routes>
       </Paper>
-    </ThemeProvider >
+    </ThemeProvider>
   );
 }
 export default App;
