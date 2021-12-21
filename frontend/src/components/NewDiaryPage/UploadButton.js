9a857af9072b745c840dfd654919c4f56a993891
allen3325 <00857146@email.ntou.edu.tw>
2021-12-21 12:15:20 +0800

    feat: add tags to createDiaryPage. chore: add axios to package.JSON.

--- frontend/src/components/NewDiaryPage/UploadButton.js
+++ frontend/src/components/NewDiaryPage/UploadButton.js
@@ -1,17 +1,17 @@
 import { Button } from "@material-ui/core";
 import styled from "@emotion/styled";
 
-const Input = styled('input')({
-    display: 'none',
-});
+const UploadButton = (props) => {
+    const Input = styled('input')({
+        display: 'none',
+    });
+    
+    const upload = () => {
+        const inputElement = document.getElementById("contained-button-file");
+        // console.log(inputElement.files[0]);
+        props.onUploadFile(inputElement.files[0]);
+    }
 
-const upload = () => {
-    const inputElement = document.getElementById("contained-button-file");
-    console.log(inputElement.files[0]);
-}
-
-
-const UploadButton = () => {
     return (
         <label htmlFor="contained-button-file">
             <Input accept="image/*" id="contained-button-file" multiple type="file" onChange={upload} />
