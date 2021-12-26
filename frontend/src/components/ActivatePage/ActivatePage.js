c106c5a621d7ab8fbe138ee1f5c9342d26762d84
allen3325 <00857146@email.ntou.edu.tw>
2021-12-26 13:11:58 +0800

    feat: add some proper redirectly and CookieParser Object.

--- frontend/src/components/ActivatePage/ActivatePage.js
+++ frontend/src/components/ActivatePage/ActivatePage.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { Paper, paperClasses } from '@mui/material';
-import Link from '@mui/material/Link';
+import { Container, Paper } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -13,8 +12,8 @@ import { Snackbar } from '@mui/material';
 export default function ActivatePage() {
   const [openFail, setOpenFail] = React.useState(false);
   const [openSuccess, setOpenSuccess] = React.useState(false);
-  let email="";
-  let code="";
+  let email = "";
+  let code = "";
   // const handleSubmit = (event) => {
   //   event.preventDefault();
   //   const data = new FormData(event.currentTarget);
@@ -24,35 +23,41 @@ export default function ActivatePage() {
   //   });
   // };
   const handleEmailChange = (event) => {
-        email = (event.target.value);
-        console.log(email);
+    email = (event.target.value);
+    console.log(email);
   }
   const handleCodeChange = (event) => {
-        code = (event.target.value);
-        console.log(code);
+    code = (event.target.value);
+    console.log(code);
   }
   const axverify = () => {
-        console.log(email);
-        console.log(code);
-        axios.post("/verify",{
-            "email":email,
-            "code":code
+    console.log(email);
+    console.log(code);
+    axios.post("/verify", {
+      "email": email,
+      "code": code
+    })
+      .then((res) => {
+        console.log(res)
+        setOpenSuccess(true);
+      })
+      .catch((error) => {
+        console.log(error)
+        setOpenFail(true)
+      })
+  }
+  const resend = () => {
+    if (email != "") {
+      axios.post("/resendCode", { email: email })
+        .then((response) => {
+          console.log(response);
+          setOpenSuccess(true);
         })
-            .then((res) => {
-                console.log(res)
-                setOpenSuccess(true);
-            })
-            .catch((error) => {
-              console.log(error)
-              setOpenFail(true)
-              })
+
+        .catch(error => console.log(error.response.status))
+    } else {
+      setOpenFail(true);
     }
-  const resend = () =>{
-        axios.post("/resendCode",{email:email})
-            .then((response) => 
-                console.log(response))
-            
-            .catch(error => console.log(error.response.status))
   }
   const handleCloseFail = (event, reason) => {
     if (reason === 'clickaway') {
@@ -68,8 +73,9 @@ export default function ActivatePage() {
     setOpenSuccess(false);
   };
   return (
-    <Paper elevation={0} style={{height:"100vh"}} >
-		<p> MyDiary </p> {""}{" "}
+    <Container maxWidth={"sm"}>
+      <Paper elevation={0} style={{ height: "100vh" }} >
+        {/* <p> MyDiary </p> */}
         <Box
           sx={{
             marginTop: 8,
@@ -78,72 +84,73 @@ export default function ActivatePage() {
             alignItems: 'center',
           }}
         >
-          
+
           <Typography component="h1" variant="h5">
             Verification
           </Typography>
           {/* <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}> */}
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="email"
-                  name="email"
-                  autoComplete="email"
-                  onChange ={handleEmailChange}
-                >
-                </TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="activate_code"
-                  label="activate_codes"
-                  name="activate_code"
-                  autoComplete="activate_code"
-                  onChange={handleCodeChange}
-                />
-              </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="email"
+                name="email"
+                autoComplete="email"
+                onChange={handleEmailChange}
+              >
+              </TextField>
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={axverify}
-            >
-              Verify
-            </Button>
-			<Typography component="h1" variant="h5">
-            	Didn't receive the verification email? 
-          	</Typography>
-			
-			<Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={resend}
-            >
-              resend
-            </Button>
-            <Grid container justifyContent="flex-end">
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="activate_code"
+                label="activate_codes"
+                name="activate_code"
+                autoComplete="activate_code"
+                onChange={handleCodeChange}
+              />
             </Grid>
-          </Box>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={axverify}
+          >
+            Verify
+          </Button>
+          <Typography component="h1" variant="h5">
+            Didn't receive the verification email?
+          </Typography>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={resend}
+          >
+            resend
+          </Button>
+          <Grid container justifyContent="flex-end">
+          </Grid>
+        </Box>
         {/* </Box> */}
-         <Snackbar open={openFail} autoHideDuration={2000} onClose={handleCloseFail}>
-        <Alert onClose={handleCloseFail} severity="error" sx={{ width: '100%' }}>
-          verify failed!!
-        </Alert>
-      </Snackbar>
-      <Snackbar open={openSuccess} autoHideDuration={2000} onClose={handleCloseSuccess}>
-        <Alert onClose={handleCloseSuccess} severity="success" sx={{ width: '100%' }}>
-          verify successfully.
-        </Alert>
-      </Snackbar>
-    </Paper>
+        <Snackbar open={openFail} autoHideDuration={2000} onClose={handleCloseFail}>
+          <Alert onClose={handleCloseFail} severity="error" sx={{ width: '100%' }}>
+            error email or error code!!
+          </Alert>
+        </Snackbar>
+        <Snackbar open={openSuccess} autoHideDuration={2000} onClose={handleCloseSuccess}>
+          <Alert onClose={handleCloseSuccess} severity="success" sx={{ width: '100%' }}>
+            successful verify!
+          </Alert>
+        </Snackbar>
+      </Paper>
+    </Container>
   );
 }
--- frontend/src/components/RegisterPage/RegisterPage.js
+++ frontend/src/components/RegisterPage/RegisterPage.js
@@ -2,8 +2,6 @@ import * as React from 'react';
 import "./RegisterPage.css";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -12,58 +10,53 @@ import { Paper } from '@mui/material';
 import axios from "../axios/axios";
 import { Alert } from '@mui/material';
 import { Snackbar } from '@mui/material';
+import { Navigate } from "react-router-dom";
+import { Container } from '@mui/material';
 
 const RegisterPage = () => {
   const [openFail, setOpenFail] = React.useState(false);
   const [openSuccess, setOpenSuccess] = React.useState(false);
-  let email="";
-  let password="";
-  let checkpassword="";
+  const [redirect, setRedirect] = React.useState(false);
+  let email = "";
+  let password = "";
+  let checkpassword = "";
   const handleEmailChange = (event) => {
-        email = (event.target.value);
-        console.log(email);
+    email = (event.target.value);
+    console.log(email);
   }
   const handlePasswordChange = (event) => {
-        password = (event.target.value);
-        console.log(password);
+    password = (event.target.value);
+    console.log(password);
   }
   const handleCheckpasswordChange = (event) => {
-        checkpassword = (event.target.value);
-        console.log(checkpassword);
+    checkpassword = (event.target.value);
+    console.log(checkpassword);
   }
   const signup = (event) => {
-    console.log("email "+email);
-    console.log("password "+ password);
-    if(password!=checkpassword){
+    // console.log("email " + email);
+    // console.log("password " + password);
+    if (password != checkpassword) {
       setOpenFail(true);
       return;
     }
-        axios.post("/signUp",{
-            email:email,
-            password:password
-        })
-            .then((response) => {
-                console.log(response)
-                setOpenSuccess(true);
-            })
-            .catch(error => {
-              console.log(error)
-              setOpenFail(true)
-            })
-        axios.post("/resendCode",{email:email})
-            .then((response) => {
-                console.log(response)
-                setOpenSuccess(true);
-                })
-            .catch(error => {
-              console.log(error)
-              setOpenFail(true)
-            })
-    }
-    // const sendVerifyCode = (event) => {
-        
-    // }  
-    const handleCloseFail = (event, reason) => {
+    axios.post("/signUp", {
+      email: email,
+      password: password
+    })
+      .then((response) => {
+        // console.log(response)
+        setOpenSuccess(true);
+      })
+      .then(() => {
+        setRedirect(true);
+      })
+      .catch(error => {
+        console.log(error)
+        setOpenFail(true)
+      })
+  }
+
+  const handleCloseFail = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
@@ -77,9 +70,10 @@ const RegisterPage = () => {
     setOpenSuccess(false);
   };
   return (
-	  
-	<Paper elevation={0} style={{height:"100vh"}} >
-		<p> MyDiary </p> {""}
+    <Container maxWidth={"sm"}>
+      <Paper elevation={0} style={{ height: "100vh" }} >
+        {redirect ? <Navigate to={"/activate"} /> : ""}
+        {/* <p> MyDiary </p> */}
         <Box
           sx={{
             marginTop: 8,
@@ -92,79 +86,80 @@ const RegisterPage = () => {
             Sign up
           </Typography>
           {/* <Box component="form" noValidate  sx={{ mt: 3 }}> */}
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  onChange={handleEmailChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  onChange={handlePasswordChange}
-                />
-              </Grid>
-			  <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="checkpassword"
-                  label="endter Password again"
-                  type="password"
-                  id="checkpassword"
-                  autoComplete="checkpassword"
-                  onChange={handleCheckpasswordChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={handleEmailChange}
+              />
             </Grid>
-            <Button
-			  // href="/activate"
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={signup}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                onChange={handlePasswordChange}
+              />
             </Grid>
-          </Box>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="checkpassword"
+                label="endter Password again"
+                type="password"
+                id="checkpassword"
+                autoComplete="checkpassword"
+                onChange={handleCheckpasswordChange}
+              />
+            </Grid>
+            {/* <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid> */}
+          </Grid>
+          <Button
+            // href="/activate"
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={signup}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
         {/* </Box> */}
         <Snackbar open={openFail} autoHideDuration={2000} onClose={handleCloseFail}>
-        <Alert onClose={handleCloseFail} severity="error" sx={{ width: '100%' }}>
-          Invaild register!!
-        </Alert>
-      </Snackbar>
-      <Snackbar open={openSuccess} autoHideDuration={2000} onClose={handleCloseSuccess}>
-        <Alert onClose={handleCloseSuccess} severity="success" sx={{ width: '100%' }}>
-          register successfully.
-        </Alert>
-      </Snackbar>
-    </Paper>
+          <Alert onClose={handleCloseFail} severity="error" sx={{ width: '100%' }}>
+            Invaild register!!
+          </Alert>
+        </Snackbar>
+        <Snackbar open={openSuccess} autoHideDuration={2000} onClose={handleCloseSuccess}>
+          <Alert onClose={handleCloseSuccess} severity="success" sx={{ width: '100%' }}>
+            register successfully.
+          </Alert>
+        </Snackbar>
+      </Paper>
+    </Container>
   );
 }
 export default RegisterPage;
