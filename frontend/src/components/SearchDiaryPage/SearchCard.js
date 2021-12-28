c95240ac72100be7d65756079d03c39652589044
allen3325 <00857146@email.ntou.edu.tw>
2021-12-28 23:48:27 +0800

    feat: search feature done Version_1 (can not see more and share).

--- frontend/src/components/SearchDiaryPage/SearchCard.js
+++ frontend/src/components/SearchDiaryPage/SearchCard.js
@@ -7,49 +7,60 @@ import { Link } from "react-router-dom";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import IconButton from '@mui/material/IconButton';
+import { useEffect,useState } from "react";
 
 const SearchCard = (props) => {
-    {console.log(props.items)}
+    const [url, setURL] = useState("");
+    let tmp = "a/";
+    useEffect(() => {
+        tmp += props.items[0].picURL[0];
+        tmp = tmp.replace('/file/d/', '/uc?id=');
+        tmp = tmp.substring(0, tmp.search('/view'));
+        tmp = tmp.replace('a/', '');
+        // console.log(tmp);
+        setURL(tmp);
+    })
+    // console.log(props.items[0])
     return (
 
-        <div>
-            adas
-        </div>
-        // <Card variant="outlined" >
-        //     <CardContent>
-        //         <Typography variant="h5" component="div" >
-        //             {props.items.title}
-        //         </Typography>
-        //         <Typography sx={{ mb: 1.5 }} color="text.secondary" component={"div"}>
-        //             {new Date(props.items.date).toDateString()}
-        //         </Typography>
-        //         {props.items.picURL.length === 0 ? "" : <CardMedia
-        //             component="img"
-        //             height="300"
-        //             image={props.url}
-        //             alt={props.items.title + "'s picture is dead."}
-        //         />
-        //         }
-        //         <div dangerouslySetInnerHTML={{ __html: props.items.markdown.substring(0, 20) }}></div>
-        //     </CardContent>
-        //     <CardActions disableSpacing>
-        //         <IconButton aria-label="add to favorites">
-        //             <FavoriteIcon />
-        //         </IconButton>
-        //         {/* <IconButton onClick={generateLink} aria-label="share">
-        //             <ShareIcon />
-        //         </IconButton> */}
-        //         <Button size="small">
-        //             <Link to={`/DiaryPage/${props.email}/${props.selectedFolder}/${props.items.title}`}><p style={{ fontSize: "1rem" }}>See More</p></Link>
-        //         </Button>
-        //         <Button size="small">
-        //             <Link to={`/editDiary/${props.email}/${props.selectedFolder}/${props.items.title}`}><p style={{ fontSize: "1rem" }}>Edit Diary</p></Link>
-        //         </Button>
-        //         {/* <Typography color="text.secondary">
-        //             {props.items.tag.length > 0 && props.items.tag[0].length > 0 ? props.items.tag.join("#") : ""}
-        //         </Typography> */}
-        //     </CardActions>
-        // </Card>
+        // <div>
+        //     adas
+        // </div>
+        <Card variant="outlined" >
+            <CardContent>
+                <Typography variant="h5" component="div" >
+                    {props.items[0].title}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary" component={"div"}>
+                    {new Date(props.items[0].date).toDateString()}
+                </Typography>
+                {props.items[0].picURL.length === 0 ? "" : <CardMedia
+                    component="img"
+                    height="300"
+                    image={url}
+                    alt={props.items[0].title + "'s picture is dead."}
+                />
+                }
+                <div dangerouslySetInnerHTML={{ __html: props.items[0].markdown }}></div>
+            </CardContent>
+            <CardActions disableSpacing>
+                {/* <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton> */}
+                {/* <IconButton onClick={generateLink} aria-label="share">
+                    <ShareIcon />
+                </IconButton> */}
+                {/* <Button size="small">
+                    <Link to={`/DiaryPage/${props.email}/${props.selectedFolder}/${props.items.title}`}><p style={{ fontSize: "1rem" }}>See More</p></Link>
+                </Button> */}
+                {/* <Button size="small">
+                    <Link to={`/editDiary/${props.email}/${props.selectedFolder}/${props.items.title}`}><p style={{ fontSize: "1rem" }}>Edit Diary</p></Link>
+                </Button> */}
+                {/* <Typography color="text.secondary">
+                    {props.items.tag.length > 0 && props.items.tag[0].length > 0 ? props.items.tag.join("#") : ""}
+                </Typography> */}
+            </CardActions>
+        </Card>
     );
 }
 export default SearchCard;
