5f26d8845ae42921772892fd69d9bb039fb5d4f9
allen3325 <00857146@email.ntou.edu.tw>
2021-12-29 22:27:13 +0800

    fix: 上傳檔案改成上傳圖片

--- frontend/src/components/NewDiaryPage/UploadButton.js
+++ frontend/src/components/NewDiaryPage/UploadButton.js
@@ -16,7 +16,7 @@ const UploadButton = (props) => {
         <label htmlFor="contained-button-file">
             <Input accept="image/*" id="contained-button-file" multiple type="file" onChange={upload} />
             <Button size="medium" variant="contained" component="span">
-                上傳檔案
+                上傳圖片
             </Button>
         </label>
     )
