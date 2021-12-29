0ffb485622ab5c14337f1a4046503a4ef78dcb31
allen3325 <00857146@email.ntou.edu.tw>
2021-12-29 22:24:21 +0800

    fix: bug4.

--- frontend/src/components/NewDiaryPage/UploadButton.js
+++ frontend/src/components/NewDiaryPage/UploadButton.js
@@ -5,7 +5,7 @@ const UploadButton = (props) => {
     const Input = styled('input')({
         display: 'none',
     });
-    
+
     const upload = () => {
         const inputElement = document.getElementById("contained-button-file");
         // console.log(inputElement.files[0]);
@@ -15,7 +15,7 @@ const UploadButton = (props) => {
     return (
         <label htmlFor="contained-button-file">
             <Input accept="image/*" id="contained-button-file" multiple type="file" onChange={upload} />
-            <Button size="medium" variant="contained" >
+            <Button size="medium" variant="contained" component="span">
                 上傳檔案
             </Button>
         </label>
